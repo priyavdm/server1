f7b2239cb0bbfd42f20aa4944ad2c890b4a8c872
priyavdm <priyavdmtpk@gmail.com>
2023-05-26 18:51:32 +0530

    new file added

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/getall',(req,res)=>{
  })
 })
 
-
+//******************insert the data**************
 app.post('/insert',(req,res)=>{
 connection.query('insert into angular (username,age,email,gender) values(?,?,?,?)',[req.body.username,req.body.age,req.body.email,req.body.gender],function(error,results){
    if(error){
